d8ca2a38405cf26b2872f260bf375536f10397ce
Danny Blackstock <dannyblackstock@gmail.com>
2014-03-27 15:44:36 -0700

    Map draws one of the paths.

--- js/draw_map.js
+++ js/draw_map.js
@@ -1,26 +1,33 @@
 function initialize() {
-    var mapOptions = {
-      center: new google.maps.LatLng(55.1552967,-126.5117003),
-      zoom: 5
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
 
-  var flightPlanCoordinates = [
-    new google.maps.LatLng(37.772323, -122.214897),
-    new google.maps.LatLng(21.291982, -157.821856),
-    new google.maps.LatLng(-18.142599, 178.431),
-    new google.maps.LatLng(-27.46758, 153.027892)
-  ];
-  var flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 2
+  var mapOptions = {
+    center: new google.maps.LatLng(55.1552967,-126.5117003),
+    zoom: 5
+  };
+
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
+
+  d3.csv("drivebc_events_hist_2012.csv", function (error, data) {
+    if (error) throw error;
+
+    console.log(data[0]);
+
+    var flightPlanCoordinates = [
+      new google.maps.LatLng(data[0]["head_latitude"], data[0]["head_longitude"]),
+      new google.maps.LatLng(data[0]["tail_latitude"], data[0]["tail_longitude"]),
+    ];
+
+    var flightPath = new google.maps.Polyline({
+      path: flightPlanCoordinates,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+
+    flightPath.setMap(map);
   });
-
-  flightPath.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
--- js/load_csv.js
+++
@@ -1,4 +0,0 @@
-d3.csv("drivebc_events_hist_2012.csv", function (error, data) {
-  if (error) throw error;
-  // alert(data);
-});

7c253e561821cbac05a644cc71340f44482e4651
Danny Blackstock <dannyblackstock@gmail.com>
2014-03-27 17:52:22 -0700

    Get some test points showing up with d3.

--- js/draw_map.js
+++ js/draw_map.js
@@ -5,28 +5,56 @@ function initialize() {
     zoom: 5
   };
 
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
+  var map = new google.maps.Map(d3.select("#map-canvas").node(),
       mapOptions);
 
-  d3.csv("drivebc_events_hist_2012.csv", function (error, data) {
-    if (error) throw error;
+  d3.csv("drivebc_events_hist_2012_subset.csv", function (error, data) {
+    var overlay = new google.maps.OverlayView();
 
-    console.log(data[0]);
+    // Add the container when the overlay is added to the map.
+    overlay.onAdd = function() {
+      var layer = d3.select(this.getPanes().overlayLayer).append("div")
+          .attr("class", "stations");
 
-    var flightPlanCoordinates = [
-      new google.maps.LatLng(data[0]["head_latitude"], data[0]["head_longitude"]),
-      new google.maps.LatLng(data[0]["tail_latitude"], data[0]["tail_longitude"]),
-    ];
+      // Draw each marker as a separate SVG element.
+      // We could use a single SVG, but what size would it have?
+      overlay.draw = function() {
+        var projection = this.getProjection(),
+            padding = 10;
 
-    var flightPath = new google.maps.Polyline({
-      path: flightPlanCoordinates,
-      geodesic: true,
-      strokeColor: '#FF0000',
-      strokeOpacity: 1.0,
-      strokeWeight: 2
-    });
+        var marker = layer.selectAll("svg")
+            .data(d3.values(data))
+            .each(transform) // update existing markers
+          .enter().append("svg:svg")
+            .each(transform)
+            .attr("class", "marker");
 
-    flightPath.setMap(map);
+        // Add a circle.
+        marker.append("svg:circle")
+            .attr("r", 4.5)
+            .attr("cx", padding)
+            .attr("cy", padding);
+
+        // Add a label.
+        marker.append("svg:text")
+            .attr("x", padding + 7)
+            .attr("y", padding)
+            .attr("dy", ".31em")
+            .text(function(d) { return d["head_latitude"]; });
+
+        function transform(d) {
+          console.log(d);
+          d = new google.maps.LatLng(d["head_latitude"], d["head_longitude"]);
+          d = projection.fromLatLngToDivPixel(d);
+          return d3.select(this)
+              .style("left", (d.x - padding) + "px")
+              .style("top", (d.y - padding) + "px");
+        }
+      };
+    };
+
+    // Bind our overlay to the map…
+  overlay.setMap(map);
   });
 }
 

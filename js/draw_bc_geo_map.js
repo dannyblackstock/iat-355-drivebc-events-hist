2bc2be852d9c11cb2421c46941e212400b9cd103
Danny Blackstock <dannyblackstock@gmail.com>
2014-04-03 21:34:37 -0700

    Add a little cdv data, change map colour

--- js/draw_bc_geo_map.js
+++ js/draw_bc_geo_map.js
@@ -1,30 +1,32 @@
-// define path generator
-var path = d3.geo.path();
+var width = 1160,
+  height = 1160;
 
-// load bc map GeoJSON file
-d3.json("bc_districts_danny_min.json", function(error, json) {
-  console.log(json);
+d3.csv("drivebc_events_hist_2012_100.csv", function (data) {
 
-  var width = 1160,
-    height = 1160;
+  // load bc map GeoJSON file
+  d3.json("bc_districts_danny_min.json", function(error, json) {
+    console.log(json);
 
-  var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    var svg = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-  var projection = d3.geo.mercator()
-    .scale((width + 12000) / 2 / Math.PI)
-    .translate([width + 8000/ 2, height + 3250 / 2])
-    .precision(0.1);
+    var projection = d3.geo.mercator()
+      .scale((width + 5420) / Math.PI)
+      .translate([width + 4000, height + 1625])
+      .precision(0.1);
 
-  var path = d3.geo.path()
-     .projection(projection);
+    var path = d3.geo.path()
+       .projection(projection);
 
-  // create a path for each feature
-  svg.selectAll("path")
-    .data(json.features)
-    .enter()
-    .append("path")
-    .attr("d", path);
+    // create a path for each feature
+    svg.selectAll("path")
+      .data(json.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("fill", "#77aaff")
+      .style("stroke", "#ffffff");
+  });
 
 });

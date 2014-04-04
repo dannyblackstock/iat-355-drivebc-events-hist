03de5bb20a33d8cc5584d55e5e2b6f6399428627
Gilbert Fung <gilbertfung@outlook.com>
2014-04-03 22:44:06 -0700

    Added basic histogram, displaying occurrences of events in the times of day

--- js/load_csv.js
+++ js/load_csv.js
@@ -1,4 +1,9 @@
 d3.csv("drivebc_events_hist_2012.csv", function (error, data) {
   if (error) throw error;
   // alert(data);
+
+  $.getScript("js/draw_histogram.js", function() {
+    // alert("draw");
+    draw_histogram(data);
+  })
 });

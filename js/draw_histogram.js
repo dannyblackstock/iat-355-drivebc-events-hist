7033ccb813c8a063b292cd1541167f31fa65a6ec
Gilbert Fung <gilbertfung@outlook.com>
2014-04-05 15:38:03 -0700

    Histogram can now change according to filtered (+narrowed too!) data. Added y axis to Histogram

--- js/draw_histogram.js
+++ js/draw_histogram.js
@@ -2,7 +2,7 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
   // id,cause,district,state,severity,localupdatetime,advisorymessage,isbidirectional,trafficpattern,head_latitude,head_longitude,tail_latitude,tail_longitude,route,type
   var attributes = Object.keys(dataset[0]); // get column names from first row
 
-  var margin = {top: 10, right: 30, bottom: 30, left: 30},
+  var margin = {top: 10, right: 30, bottom: 30, left: 50},
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
@@ -12,46 +12,46 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+  draw_histogram(null, null);
   selectField("#select-timescale", ["24 hours"]);
   selectField("#select-category", ["All events", "type", "trafficpattern", "district"]);
-  // selectField("#select-category-values", ["All events", getUnique("type"), getUnique("trafficpattern"), getUnique("district")]);
-  draw_histogram(null, null);
   
-  function draw_histogram(timescale, category) { 
+  d3.select("#select-category-values").on("change", function() {
+    cleanUp();
+    draw_histogram(null, $("#select-category").val(), $(this).val());
+  });
+
+  function draw_histogram(timescale, category, narrowCat) { 
     // http://bl.ocks.org/mbostock/3048450
     var values = []; // values to be put into bins
     // var timeDateFormat = d3.time.format("%x %_H:%M"); // 01/31/2012 0:30
     // var timeFormat = d3.time.format("%_H:%M"); // 0:30
     // var dateFormat = d3.time.format("%x"); // 01/31/2012
 
-    switch (category) {
-      case "type":
-        // add to times where type is (selectedindropdown)
-        for (var i = 0; i < dataset.length; i++) {
-          if (dataset[i][""] == d3.select("#select-category-values")) { // datum matches all types in multiselect
-            var time = timeToDecimal(dataset[i]["localupdatetime"].substring(10));
-            values.push(time);
-          }
-        }
-        break;
-      case "trafficpattern":
-        // execute code block 2
-        break;
-      case "district":
-        // execute code block 2
-        break;
-      default: // All events
-        for (var i = 0; i < dataset.length; i++) {
+    // Adds only what is in the category to the data.
+    if (category == "All events" || category == null) {
+      for (var i = 0; i < dataset.length; i++) {
+        var time = timeToDecimal(dataset[i]["localupdatetime"].substring(10));
+        // time = timeFormat.parse(time);
+        values.push(time);
+      }
+    } else if (narrowCat) {
+      for (var i = 0; i < dataset.length; i++) {
+        if ($.inArray(dataset[i][category], narrowCat)) {
           var time = timeToDecimal(dataset[i]["localupdatetime"].substring(10));
-          // time = timeFormat.parse(time);
           values.push(time);
         }
+      }
+    } else {
+      var options = getUnique(category);
+      for (var i = 0; i < dataset.length; i++) {
+        if ($.inArray(dataset[i][category], options)) {
+          var time = timeToDecimal(dataset[i]["localupdatetime"].substring(10));
+          values.push(time);
+        }
+      }
     }
 
-    function timeToDecimal(t) {
-      var arr = t.split(':');
-      return parseFloat(parseInt(arr[0], 10) + '.' + parseInt((arr[1]/6)*10, 10));
-    }
 
     // A formatter for counts.
     var formatCount = d3.format(",.0f");
@@ -66,12 +66,16 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
         (values);
 
     var y = d3.scale.linear()
-        .domain([0, d3.max(data, function(d) { return d.y; })])
+        .domain([0, 20000])//d3.max(data, function(d) { return d.y; })])
         .range([height, 0]);
 
     var xAxis = d3.svg.axis()
         .scale(x)
         .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
 
     // var svg = d3.select("#histogram-canvas").append("svg")
     //     .attr("width", width + margin.left + margin.right)
@@ -101,6 +105,11 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(0, 0)")
+        .call(yAxis);
   }
 
   function selectField (id, options) {
@@ -132,11 +141,13 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
 
         multiSelect.selectAll("option").property('selected', true);
 
+        cleanUp();
         draw_histogram(null, this.value);
       } else if (id == "#select-timescale") {
+        cleanUp();
         draw_histogram(this.value, null);
       }
-      console.log("You selected: " + this.value);
+      // console.log("You selected: " + this.value);
     });
   }
 
@@ -151,5 +162,16 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
     })
 
     return uniques.sort(); // get unique attribute values (for filters)
+  }
+
+  function timeToDecimal(t) {
+    var arr = t.split(':');
+    return parseFloat(parseInt(arr[0], 10) + '.' + parseInt((arr[1]/6)*10, 10));
+  }
+
+  function cleanUp() {
+    svg.selectAll(".bar").remove();
+    svg.selectAll(".x.axis").remove();
+    svg.selectAll(".y.axis").remove();
   }
 });

8d655e92fe6c2599e04d7804b903f089538d8aff
Gilbert Fung <gilbertfung@outlook.com>
2014-04-07 16:29:55 -0700

    Histogram can now filter according to Time scale (in addition to categories). Though... Still not sure if the graphs are using the right data.

--- js/draw_histogram.js
+++ js/draw_histogram.js
@@ -1,6 +1,6 @@
 d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
   // id,cause,district,state,severity,localupdatetime,advisorymessage,isbidirectional,trafficpattern,head_latitude,head_longitude,tail_latitude,tail_longitude,route,type
-  var attributes = Object.keys(dataset[0]); // get column names from first row
+  // var attributes = Object.keys(dataset[0]); // get column names from first row
 
   var margin = {top: 10, right: 30, bottom: 30, left: 50},
       width = 960 - margin.left - margin.right,
@@ -11,53 +11,55 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
         .attr("height", height + margin.top + margin.bottom)
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  draw_histogram(null, null);
-  selectField("#select-timescale", ["24 hours"]);
-  selectField("#select-category", ["All events", "type", "trafficpattern", "district"]);
   
+  var timescaleOptions = ["Time of day", "Day of week", "Monthly"],
+      categoryOptions = ["All events", "type", "trafficpattern", "district"];
+
+  selectField("#select-timescale", timescaleOptions);
+  selectField("#select-category", categoryOptions);
   d3.select("#select-category-values").on("change", function() {
     cleanUp();
-    draw_histogram(null, $("#select-category").val(), $(this).val());
+    draw_histogram($("#select-timescale").val(), $("#select-category").val(), $(this).val());
   });
+
+  draw_histogram("Time of day", "All events");
+  
 
   function draw_histogram(timescale, category, narrowCat) { 
     // http://bl.ocks.org/mbostock/3048450
     var values = []; // values to be put into bins
-    // var timeDateFormat = d3.time.format("%x %_H:%M"); // 01/31/2012 0:30
-    // var timeFormat = d3.time.format("%_H:%M"); // 0:30
-    // var dateFormat = d3.time.format("%x"); // 01/31/2012
+    var xRange, yRange;
 
     // Adds only what is in the category to the data.
-    if (category == "All events" || category == null) {
-      for (var i = 0; i < dataset.length; i++) {
-        var time = timeToDecimal(dataset[i]["localupdatetime"].substring(10));
-        // time = timeFormat.parse(time);
-        values.push(time);
-      }
-    } else if (narrowCat) {
+    switch (category) {
+      case "All events":
+        for (var i = 0; i < dataset.length; i++) {
+          values.push(setTimescale(i, timescale));
+        }
+        break;
+      default:
+        var options = getUnique(category);
+        for (var i = 0; i < dataset.length; i++) {
+          if ($.inArray(dataset[i][category], options)) {
+            values.push(setTimescale(i, timescale));
+          }
+        }
+    }
+
+    if (narrowCat) { // a particular narrowed filter is selected
+      values = [];
       for (var i = 0; i < dataset.length; i++) {
         if ($.inArray(dataset[i][category], narrowCat)) {
-          var time = timeToDecimal(dataset[i]["localupdatetime"].substring(10));
-          values.push(time);
-        }
-      }
-    } else {
-      var options = getUnique(category);
-      for (var i = 0; i < dataset.length; i++) {
-        if ($.inArray(dataset[i][category], options)) {
-          var time = timeToDecimal(dataset[i]["localupdatetime"].substring(10));
-          values.push(time);
+          values.push(setTimescale(i, timescale));
         }
       }
     }
-
 
     // A formatter for counts.
     var formatCount = d3.format(",.0f");
 
     var x = d3.scale.linear()
-        .domain([0, 24])
+        .domain([0, xRange])
         .range([0, width]);
 
     // Generate a histogram using twenty uniformly-spaced bins.
@@ -66,7 +68,7 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
         (values);
 
     var y = d3.scale.linear()
-        .domain([0, 20000])//d3.max(data, function(d) { return d.y; })])
+        .domain([0, yRange])// d3.max(data, function(d) { return d.y; })])
         .range([height, 0]);
 
     var xAxis = d3.svg.axis()
@@ -76,12 +78,6 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
     var yAxis = d3.svg.axis()
         .scale(y)
         .orient("left");
-
-    // var svg = d3.select("#histogram-canvas").append("svg")
-    //     .attr("width", width + margin.left + margin.right)
-    //     .attr("height", height + margin.top + margin.bottom)
-    //   .append("g")
-    //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var bar = svg.selectAll(".bar")
         .data(data)
@@ -110,6 +106,27 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
         .attr("class", "y axis")
         .attr("transform", "translate(0, 0)")
         .call(yAxis);
+
+    function setTimescale (i, timescale) {
+      var time;
+      switch (timescale) {
+        case "Day of week":
+          time = new Date(dataset[i]["localupdatetime"]).getDay();
+          xRange = 7;
+          yRange = 80000;
+          break;
+        case "Monthly":
+          time = new Date(dataset[i]["localupdatetime"]).getMonth();
+          xRange = 12;
+          yRange = 40000;
+          break;
+        default: // "Time of day" as default
+          time = new Date(dataset[i]["localupdatetime"]).getHours();
+          xRange = 24;
+          yRange = 20000;
+      }
+      return time;
+    }
   }
 
   function selectField (id, options) {
@@ -138,14 +155,13 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
             .data(getUnique(this.value))
           .enter().append("option")
             .text(String);
-
         multiSelect.selectAll("option").property('selected', true);
 
         cleanUp();
-        draw_histogram(null, this.value);
+        draw_histogram($("#select-timescale").val(), this.value);
       } else if (id == "#select-timescale") {
         cleanUp();
-        draw_histogram(this.value, null);
+        draw_histogram(this.value, $("#select-category").val());
       }
       // console.log("You selected: " + this.value);
     });
@@ -164,11 +180,7 @@ d3.csv("drivebc_events_hist_2012.csv", function (error, dataset) {
     return uniques.sort(); // get unique attribute values (for filters)
   }
 
-  function timeToDecimal(t) {
-    var arr = t.split(':');
-    return parseFloat(parseInt(arr[0], 10) + '.' + parseInt((arr[1]/6)*10, 10));
-  }
-
+  
   function cleanUp() {
     svg.selectAll(".bar").remove();
     svg.selectAll(".x.axis").remove();
